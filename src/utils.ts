084a7de0374cefe1077b7ceb2c954b148f2e76bf
Israel Abazie <you@example.com>
2024-07-25 06:49:33 +0100

    Create debounce function in utils.ts file

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,16 @@ export const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
   };
+
+  export const debounce = (func: Function, delay: number) => {
+    let timerId: NodeJS.Timeout;
+    return (...args: any[]) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
   
 
   

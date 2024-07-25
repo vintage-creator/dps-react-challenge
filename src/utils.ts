5f1eec0181039d7fec87ee363eed5ea8fde432c2
Israel Abazie <you@example.com>
2024-07-25 06:47:14 +0100

    Create utility function

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+  };
+  
+
+  
